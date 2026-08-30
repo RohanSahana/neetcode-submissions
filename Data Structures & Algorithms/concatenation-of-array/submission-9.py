8a8f11aaf7b71dd2646d6e7f69a15e4a510f66a7
RohanSahana <64703711+RohanSahana@users.noreply.github.com>
2026-08-30 16:55:42 +0530

    Add: concatenation-of-array - submission-9

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-9.py
@@ -0,0 +1,8 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        ans = []
+        count = 2
+        while count > 0:
+            ans.extend(nums)
+            count -= 1
+        return ans
