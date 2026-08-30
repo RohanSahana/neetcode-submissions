9eb8dc7dd148916877d360bb43e638515fd3e7cf
RohanSahana <64703711+RohanSahana@users.noreply.github.com>
2026-08-31 00:33:44 +0530

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for idx1,i in enumerate(nums):
+            for idx2,j in enumerate(nums):
+                if idx1 == idx2:
+                    continue
+                if i + j == target:
+                    return [idx1,idx2]
